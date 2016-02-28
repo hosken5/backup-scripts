cfee1e778284b58b06b0b7b526ff264b6837cb6c
hongpengfei <hongpengfei@yimei180.com>
2016-02-28 11:39:27 +0800

    1

--- src/backup_database.sh
+++ src/backup_database.sh
@@ -4,7 +4,7 @@
 mkdir  -p ~/backup/prod/database
 
 
-rsync -azrlP   -e "ssh -p 26622"  mysql@kitt1:/home/mysql/backup  ~/backup/prod/database
+rsync -azrlP   -e "ssh -p 26622"  kitt@ali4:/home/kitt/backup  ~/backup/prod/database
 #rsync -azrP --exclude ''  kitt@kittc:~/source/20151020-152521  ~/backup/prod/source/
 echo "同步成功！"
 if [ $? != 0 ]
