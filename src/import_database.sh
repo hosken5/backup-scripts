2c3072ae652c818708d89f5378405819ddad35c8
hongpengfei <hongpengfei@yimei180.com>
2016-02-28 17:04:16 +0800

    1

--- src/import_database.sh
+++ src/import_database.sh
@@ -2,6 +2,7 @@
 
 cd /home/jenkins/backup/prod/database/backup
 echo "开始导入kitt.$(date +%Y%m%d)_04_04.sql到数据库。。。"
-mysql -udemo -p'1qaz@WSX' kitt1 <kitt.$(date +%Y%m%d)_04_04.sql
+gunzip -c  kitt.$(date +%Y%m%d)_04_04.sql.gz > kitt.$(date +%Y%m%d)_04_04.sql
+mysql -udemo -p'1qaz@WSX' kitt1 <   kitt.$(date +%Y%m%d)_04_04.sql
  # mysql -udemo -p'1qaz@WSX' kitt1 <kitt.20151110_04_04.sql
 echo "导入成功！"
